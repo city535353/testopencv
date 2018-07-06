87e2b228ed21b085a5b2ab4174c7810f23881501
city535353 <city535353@gmail.com>
2018-07-06 18:25:57 +0800

    t2
    t2

--- sketch.js
+++ sketch.js
@@ -14,45 +14,9 @@ let video;
 
 function setup() {
   noCanvas();
-  
-// 定義要取得的影音內容，包含影像和聲音
-let constraints = {
-  audio: false,
-  //video: { facingMode: { exact: "environment" } }
-}
-  
-let inputVideo = document.querySelector('#inputVideo')
-
-navigator.mediaDevices.getUserMedia(constraints)
-  .then(function (stream) {
-    inputVideoURL = URL.createObjectURL(stream)
-    inputVideo.src = inputVideoURL
-    inputVideo.controls = false       // 要不要顯示播放控制器
-  })
-  .catch(function (error) {
-    console.warn('some error occurred' + error)
-  });
-  
-inputVideo.play();
-  
-  /* Create a KNN Image Classifier
-  KNNImageClassifier(?numClasses, ?knnKValue, callback, ?video)
-  
-  callback - A function to run once the model has been loaded.
-
-  numClasses - The number of classes to be able to detect. Optional, defaults to 15.
-
-  knnKValue - The number of nearest neighbors to look at when predicting. Optional, defaults to 5.
-
-  video - A HTMLVideoElement
-  
-  */
-  
-  
-  
-//  videodom = createCapture(VIDEO).parent('videoContainer');
-
-  knn = new ml5.KNNImageClassifier(2, 1, modelLoaded, inputVideo);
+  video = createCapture(VIDEO).parent('videoContainer');
+  // Create a KNN Image Classifier
+  knn = new ml5.KNNImageClassifier(2, 1, modelLoaded, video.elt);
   createButtons();
 }
 
